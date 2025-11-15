0d7fbf4d19cda7f7b7d458dc3bb3210dde726466
Mahi.K <katikelamanaswani9969p@gmail.com>
2025-11-16 00:08:38 +0530

    Add random affirmations generator in affirmations.py

---
+++ fun/affirmations.py
@@ -0,0 +1,12 @@
+import random
+
+messages = [
+    "You're not behind. You're buffering.",
+    "Depth > noise. Always.",
+    "Tiny commits still count.",
+    "You don’t need to overshare to shine.",
+    "Building quietly is still building."
+]
+
+if __name__ == "__main__":
+    print(random.choice(messages))
